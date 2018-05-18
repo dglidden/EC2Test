0a9534d736ff620061bfa28905ab146aac93b2c1
Derek Glidden <derek.glidden@avalonhcs.com>
2018-05-18 14:21:17 -0400

    Commit

--- src/main/java/com/illusionary/EC2Test.java
+++ src/main/java/com/illusionary/EC2Test.java
@@ -1,24 +1,24 @@
 package com.illusionary;
 
 import com.amazonaws.util.EC2MetadataUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class EC2Test implements CommandLineRunner {
 
-    private final static Logger LOGGER = Logger.getLogger(EC2Test.class.getName());
+    private final static Logger LOGGER = LoggerFactory.getLogger(EC2Test.class);
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
-        LOGGER.log(Level.INFO, "Getting EC2 MetaData");
+        LOGGER.info("Getting EC2 MetaData");
         EC2MetadataUtils.InstanceInfo instanceInfo = EC2MetadataUtils.getInstanceInfo();
 
         String accountId = instanceInfo.getAccountId();
@@ -32,29 +32,33 @@ public class EC2Test implements CommandLineRunner {
         String region = instanceInfo.getRegion();
         String version = instanceInfo.getVersion();
 
-        LOGGER.log(Level.INFO, "Instance Info");
-        LOGGER.log(Level.INFO, "Account ID: {0}", accountId);
-        LOGGER.log(Level.INFO, "Architecture: {0}", architecture);
-        LOGGER.log(Level.INFO, "Availability Zone: {0}", availabilityZone);
-        LOGGER.log(Level.INFO, "Image ID: {0}", imageId);
-        LOGGER.log(Level.INFO, "Instance ID: {0}", instanceId);
-        LOGGER.log(Level.INFO, "Instance Type: {0}", instanceType);
-        LOGGER.log(Level.INFO, "Kernel ID: {0}", kernelId);
-        LOGGER.log(Level.INFO, "Private Address: {0}", privateAddress);
-        LOGGER.log(Level.INFO, "Region: {0}", region);
-        LOGGER.log(Level.INFO, "Version: {0}", version);
+        LOGGER.info("Instance Info");
+        LOGGER.info("Account ID: {0}", accountId);
+        LOGGER.info("Architecture: {0}", architecture);
+        LOGGER.info("Availability Zone: {0}", availabilityZone);
+        LOGGER.info("Image ID: {0}", imageId);
+        LOGGER.info("Instance ID: {0}", instanceId);
+        LOGGER.info("Instance Type: {0}", instanceType);
+        LOGGER.info("Kernel ID: {0}", kernelId);
+        LOGGER.info("Private Address: {0}", privateAddress);
+        LOGGER.info("Region: {0}", region);
+        LOGGER.info("Version: {0}", version);
 
         Map<String, EC2MetadataUtils.IAMSecurityCredential> iam = EC2MetadataUtils.getIAMSecurityCredentials();
         EC2MetadataUtils.IAMSecurityCredential sec = iam.get("RLEADEVCE");
-        LOGGER.log(Level.INFO, "IAM Info");
-        LOGGER.log(Level.INFO, "AccessKeyId: {0}", sec.accessKeyId);
-        LOGGER.log(Level.INFO, "Code: {0}", sec.code);
-        LOGGER.log(Level.INFO, "Expiration: {0}", sec.expiration);
-        LOGGER.log(Level.INFO, "Last Updated: {0}", sec.lastUpdated);
-        LOGGER.log(Level.INFO, "Message: {0}", sec.message);
-        LOGGER.log(Level.INFO, "Secret Access Key: {0}", sec.secretAccessKey);
-        LOGGER.log(Level.INFO, "Token: {0}", sec.token);
-        LOGGER.log(Level.INFO, "Type: {0}", sec.type);
+        if(sec != null) {
+            LOGGER.info("IAM Info");
+            LOGGER.info("AccessKeyId: {0}", sec.accessKeyId);
+            LOGGER.info("Code: {0}", sec.code);
+            LOGGER.info("Expiration: {0}", sec.expiration);
+            LOGGER.info("Last Updated: {0}", sec.lastUpdated);
+            LOGGER.info("Message: {0}", sec.message);
+            LOGGER.info("Secret Access Key: {0}", sec.secretAccessKey);
+            LOGGER.info("Token: {0}", sec.token);
+            LOGGER.info("Type: {0}", sec.type);
+        } else {
+            LOGGER.info("No IAM Information Available");
+        }
     }
 
     public static void main(String[] args) {
